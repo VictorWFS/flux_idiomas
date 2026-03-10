be0afaa6aa0a2e2bf2b2ec6f2bea62c167ed2d1d
VictorWFS <victorwfs900@gmail.com>
2026-03-10 19:19:01 -0300

    feat: melhorias na tela de login

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -6,15 +6,47 @@ import Image from 'next/image';
 export default function LoginForm() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [errors, setErrors] = useState<{ email? :string; password? :string; }> ({});
 
-    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
 
-        console.log({
-            email,
-            password,
-        });
+    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault(); //impede o refresh da página
+        const isValid = validateForm();
+
+        if (!isValid) return;
+
+        setIsLoading(true);
+
+        await new Promise((resolve) => setTimeout(resolve, 1500));
+
+        setIsLoading(false);
+
+        console.log("Formulário válido");
+        
     }
+
+    function validateForm() {
+        const newErrors: {
+            email? :string;
+            password? :string;
+        } = {};
+
+        if(!email.trim()) {
+            newErrors.email = "O email é obrigatório.";
+        }
+
+        if (!password.trim()) {
+            newErrors.email = "A senha é obrigatória.";
+        }
+
+        setErrors(newErrors);
+
+        return Object.keys(newErrors).length === 0;
+    }
+
+
 
     return (
         <div className='w-full max-w-md rounded-2xl border border-white/10 bg-black/40 p-8 pt-3 shadow-2xl backdrop-blur-md'>
@@ -40,6 +72,9 @@ export default function LoginForm() {
                     onChange={(e) => setEmail(e.target.value)}
                     className='w-full rounded-xl border border-white/10 bg-white/5 px-4 py-3 text-white outline-none placeholder:text-white/40 focus:border-blue-700' 
                     />
+                    {errors.email && (
+                        <p className='mt-2 text-sm text-red-400'>{errors.email}</p>
+                    )}
                 </div>
                 
                 <div>
@@ -47,11 +82,24 @@ export default function LoginForm() {
                         Senha                      
                     </label>
                     <input 
-                    type="password"
+                    type= {showPassword ? 'text' : 'password'}
                     placeholder='Digite sua senha'
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     className='w-full rounded-xl border border-white/10 bg-white/5 px-4 py-3 text-white outline-none placeholder:text-white/40 focus:border-blue-700'/>
+
+                    {errors.password && (
+                        <p className=' mt-2 text-sm text-red-400'>{errors.password}</p>
+                    )}
+                    
+                    
+                    <button type = "button"
+                    onClick={() => setShowPassword((prev) => !prev)}
+                    className='text-white cursor-pointer mt-2 ml-1 hover:text-blue-800'
+                    >
+                        {showPassword ? 'Ocultar senha' : 'Mostrar senha'}
+                    </button>
+
                 </div>
 
                 <div className='flex items-center justify-between text-sm'>
@@ -67,8 +115,9 @@ export default function LoginForm() {
                 <button
                     type="submit"
                     className="w-full rounded-xl bg-blue-900 px-4 py-3 font-medium text-white transition hover:bg-blue-800 cursor-pointer"
+                    disabled={isLoading}
                 >
-                    Entrar
+                    {isLoading ? 'Entrando...' : 'Entrar'}
                 </button>
 
             </form>
